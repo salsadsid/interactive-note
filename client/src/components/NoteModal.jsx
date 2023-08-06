1555b7a94c38010339ac7f72e57ec76971058eb2
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-08-07 02:28:13 +0600

    add-note-modal

--- client/src/components/NoteModal.jsx
+++ client/src/components/NoteModal.jsx
@@ -1,32 +1,155 @@
-import React from 'react';
-import { Button, Modal } from 'flowbite-react';
+import React, { useState } from "react";
+import { Button, Modal } from "flowbite-react";
+import { useForm } from "react-hook-form";
 
-const NoteModal = ({props}) => {
-    console.log(props)
-    return (
-        <Modal show={props.openModal === 'default'} onClose={() => props.setOpenModal(undefined)}>
-        <Modal.Header>Terms of Service</Modal.Header>
-        <Modal.Body>
-          <div className="space-y-6">
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-              companies around the world are updating their terms of service agreements to comply.
-            </p>
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to
-              ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as
-              possible of high-risk data breaches that could personally affect them.
-            </p>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => props.setOpenModal(undefined)}>I accept</Button>
-          <Button color="gray" onClick={() => props.setOpenModal(undefined)}>
-            Decline
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    );
+const NoteModal = ({ props }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const [error, setError] = useState({
+    isError: false,
+    message: "",
+  });
+  const onSubmit = (data) => {
+    // console.log(data);
+    setError({
+      isError: false,
+      message: "",
+    });
+    const formData = new FormData();
+    // console.log(data?.file?.length)
+    if (data.file?.length) {
+      const file = data.file[0];
+      if (file.size > 200800) {
+        setError({
+          isError: true,
+          message: "Maximun size: 200 Kb",
+        });
+        return;
+      }
+      if (
+        file.type == "image/webp" ||
+        file.type == "image/jpeg" ||
+        file.type == "image/png"
+      ) {
+        formData.append("image", data.file[0]);
+      }else{
+        setError({
+            isError: true,
+            message: "Only png, jpeg, webp files are valid.",
+          });
+          return;
+      }
+    }
+    formData.append("title", data.title);
+    formData.append("body", data.body);
+    console.log(formData);
+    //   fetch("", {
+    //     method: "POST",
+    //     body: formData,
+    //   })
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       if(data.success){
+    //         reset()
+    //         alert("Successfully Added.");
+    //       }
+    //     });
+    // } else {
+    //   setError({
+    //     isError: true,
+    //     message: "Only png, jpeg, webp files are valid.",
+    //   });
+    //   return;
+  };
+  return (
+    <Modal
+      show={props.openModal === "default"}
+      onClose={() => props.setOpenModal(undefined)}
+    >
+      <Modal.Header>Add Note</Modal.Header>
+      <Modal.Body>
+        <div className="space-y-6">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="mb-6">
+              <label
+                for="default-input"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Title
+              </label>
+              <input
+                {...register("title", {
+                  required: "Title is required",
+                })}
+                type="text"
+                id="default-input"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+              {errors.title?.message && (
+                <small className="text-orange-700">
+                  {errors.title.message}
+                </small>
+              )}
+            </div>
+            <div>
+              <label
+                for="message"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Body
+              </label>
+              <textarea
+                {...register("body", {
+                  required: "Body is required",
+                })}
+                id="message"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your thoughts here..."
+              ></textarea>
+              {errors.body?.message && (
+                <small className="text-orange-700">{errors.body.message}</small>
+              )}
+            </div>
+            <div>
+              <label
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                for="file_input"
+              >
+                Upload file
+              </label>
+              <input
+                {...register("file")}
+                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                id="file_input"
+                type="file"
+              />
+              {error.isError && (
+                <small className="text-orange-700">{error.message}</small>
+              )}
+              {errors.file?.message && (
+                <small className="text-orange-700">{errors.file.message}</small>
+              )}
+            </div>
+            <input
+              type="submit"
+              class="mt-6 hover:cursor-pointer focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            />
+          </form>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={() => props.setOpenModal(undefined)}>I accept</Button>
+        <Button color="gray" onClick={() => props.setOpenModal(undefined)}>
+          Decline
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default NoteModal;
---
+++ server/models/Note.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const noteSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      validate: [validator.isEmail, "Provide a valid Email"],
+      trim: true,
+      lowercase: true,
+      required: [true, "Email is required"],
+    },
+    img: {
+      data: Buffer,
+      contentType: String,
+      filename: String,
+      path: String,
+      size: String,
+      destination: String,
+    },
+    category: {
+      type: String,
+      enum: ["home", "office", "personal","other"],
+      default: "other",
+      required: [true, "Category is required"],
+    },
+    title:{
+        type:String,
+        required: [true, "Title is required"],
+    },
+    body:{
+        type:String,
+        required: [true, "Body is required"],
+    }
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.models.Note || mongoose.model("Note", noteSchema);
+
+module.exports = Note;
