2839fd6f9e1900ca6d8bda62a9115ad97e8bef7b
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-09-17 18:58:41 +0600

    redux-toolkit-boilerplate-added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import AuthProvider from "./context/AuthProvider.jsx";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
       <AuthProvider>
         <App />
       </AuthProvider>
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
